f6523ea359972fbd513c1a433048651059a6da08
wangrui <15812829247@163.com>
2019-05-25 13:23:45 +0800

    git测试

--- src/main/java/com/catchsite/CatchApp.java
+++ src/main/java/com/catchsite/CatchApp.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CatchApp {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CatchApp.class, args); 
 	}
